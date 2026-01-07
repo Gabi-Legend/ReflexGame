909856b3cf2670b8000551bcc77dccf2571d4b45
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-07 14:34:47 +0200

    button added + game functionality

--- reflex-game.ino
+++ reflex-game.ino
@@ -1,16 +1,24 @@
 int LED = 2;
+int BUTTON = 3;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
   Serial.begin(9600);
-
 }
 
 void loop() {
-  delay(1000);
-  int wait = random(200, 5000);
+  int wait = random(300, 5000);
+  delay(wait);           
+
   digitalWrite(LED, HIGH);
-  delay(2000);
-  digitalWrite(LED, LOW);
-  delay(2000);
+  unsigned long start_time = millis();
+  while (digitalRead(BUTTON) == HIGH) {
+  }
+
+  unsigned long reaction_time = millis() - start_time;
+  Serial.println(reaction_time);
+
+  digitalWrite(LED, LOW);   
+  delay(1000);             
 }
